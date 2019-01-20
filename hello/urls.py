b48f12f6d833080fe0b8a1376e60213453cfbcdd
zeroam <imdff0803@gmail.com>
2019-01-20 14:39:07 +0900

    'django tutorial'

---
+++ hello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+  path("", views.HomeListView.as_view(), name="home"),
+  path("hello/<name>", views.hello_there, name="hello_there"),
+  path("about/", views.about, name="about"),
+  path("contact/", views.contact, name="contact"),
+  path("log/", views.log_message, name="log")
+]
